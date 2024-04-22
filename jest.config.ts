b81b99a65fab97325ca138a43370fa72c3b7ca25
moayaan1911 <ayaangames@gmail.com>
2024-04-23 02:16:25 +0530

    final commit

--- jest.config.ts
+++ jest.config.ts
@@ -6,5 +6,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/app/**/*.ts'],
 };
 export default config;
